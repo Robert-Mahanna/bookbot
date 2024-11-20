3be9396687208e67540186f8d40ed626fb57d237
Robert-Mahanna <robertcmahannajr@gmail.com>
2024-11-19 23:24:50 -0600

    Created character counting function

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
+# Function to open and read a file from a directory
 def get_book_text(path):
     with open(path) as f:
         return f.read()
     
+# Function to count the number of words in a string; split by white space
 def get_word_count(text):
     words = text.split()
     return len(words)
 
+# Function to count the number of times each character appears in a string
+def get_character_count(text):
+    character_counter = {}
+    for character in text:
+        lowered = character.lower()
+        if lowered in character_counter:
+            character_counter[lowered] += 1
+        else:
+            character_counter[lowered] = 1
+    return character_counter
+
+
 def main():
-    # Change later to utilize user input
+    # TODO: Change book_path to utilize user input
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+    character_count = get_character_count(text)
     word_count = get_word_count(text)
     
-    # Print statement for error checking
-    print(f"Found {word_count} words in this document.")
+    # Print statements for error checking
+    # print(text)
+    # print(f"Found {word_count} words in this document.")
+    print(character_count)
 
 main()
